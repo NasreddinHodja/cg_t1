f64d73ef6f413705549dcf09d6c257fa6d623218
NasreddinHodja <tbizetde@gmail.com>
2021-08-14 19:40:47 -0300

    bounding box; circle raster

--- rasterization.py
+++ rasterization.py
@@ -13,11 +13,7 @@ import sys
 
 import json
 import numpy as np
-# import scipy as sp
-# from google.colab.patches import cv2_imshow
-# from google.colab import files
-# from matplotlib import patheffects
-# import matplotlib.pyplot as plt
+from PIL import Image
 
 def inside(x, y, primitive):
     """
@@ -26,6 +22,7 @@ def inside(x, y, primitive):
     Args:
         x (float): horizontal point position
         y (float): vertical point position
+        primitive (dict): primitive shape
     Returns:
         True if (x,y) is inside the primitive, False case contrary
     """
@@ -33,10 +30,46 @@ def inside(x, y, primitive):
     # You should implement your inside test here for all shapes
     # for now, it only returns a false test
 
+    if primitive["shape"] == "circle":
+        dist_sqr = ((primitive["center"][0] - x) ** 2 +
+                    (primitive["center"][1] - y) ** 2)
+
+        return dist_sqr <= primitive["radius"] ** 2
+    else:
+
+
     return False
+
+def bounding_box(primitive):
+    """ Creates a bounding box for a given circle ou convex polygon
+
+    Args:
+        primitive (dict): primitive shape
+    Returns:
+        [[x1, y1], [x2, y2]] corresponding do the bounding box, where
+
+        (x1, y1) X-----+
+                 |     |
+                 +-----X (x2, y2)
+    """
+
+    if primitive["shape"] == "circle":
+        bbox = [[primitive["center"][0] - primitive["radius"],
+                 primitive["center"][1] - primitive["radius"]],
+                [primitive["center"][0] + primitive["radius"],
+                 primitive["center"][1] + primitive["radius"]]]
+    else:
+        x_coords, y_coords = zip(*primitive["vertices"])
+        bbox = [[min(x_coords), min(y_coords)],
+                [max(x_coords), max(y_coords)]]
+
+    primitive["bounding_box"] = bbox
+    return primitive
+
 
 class Screen:
     """ Creates a virtual basic screen
+
 
     Args:
         gdata (dict): dictionary containing screen size and scene description
@@ -64,10 +97,7 @@ class Screen:
         preprop_scene = []
 
         for primitive in scene:
-            # do some processing
-            # for now, only copies each primitive to a new list
-
-            preprop_scene.append(primitive)
+            preprop_scene.append(bounding_box(primitive))
 
         return preprop_scene
 
@@ -80,21 +110,26 @@ class Screen:
 
         self._image = 255 *  np.ones((self._height, self._width, 3), np.uint8)
 
-        def rasterize(self):
-            """ Rasterize the primitives along the Screen
-            """
+    def rasterize(self):
+        """ Rasterize the primitives along the Screen
+        """
 
-            for primitive in self._scene:
-                # Loop through all pixels
-                # You MUST use bounding boxes in order to speed up this loop
-                for w in range(0, self._width):
-                    x = w + 0.5
-                    for h in range(0, self._height):
-                        y = h + 0.5
-                        # First, we check if the pixel center is inside the primitive
-                        if (inside(x, y, primitive)):
-                            im_x, im_y = w, self._height - (h + 1)
-                            self._image[im_y, im_x] = primitive["color"][::-1]
+        for primitive in self._scene:
+            print(primitive)
+            bbox = primitive["bounding_box"]
+            print(bbox)
+            # Loop through all pixels
+            # You MUST use bounding boxes in order to speed up this loop
+            for w in range(bbox[0][0], bbox[1][0]):
+                x = w + 0.5
+                for h in range(bbox[0][1], bbox[1][1]):
+                    y = h + 0.5
+                    # First, we check if the pixel center is inside the primitive
+                    im_x, im_y = w, self._height - (h + 1)
+                    if inside(x, y, primitive):
+                        self._image[im_y, im_x] = primitive["color"][::-1]
+                    else:
+                        self._image[im_y, im_x] = [100, 0, 0]
 
 
     def show(self, exec_rasterize = False):
@@ -104,18 +139,14 @@ class Screen:
         if (exec_rasterize):
             self.rasterize()
 
-        cv2_imshow(self._image)
+        Image.fromarray(self._image).show()
 
 def main():
-    try:
-        with open(sys.argv[1]) as json_f:
-            graphic_data = json.load(json_f)
+    with open(sys.argv[1]) as json_f:
+        graphic_data = json.load(json_f)
 
-        # screen = Screen(graphic_data)
-        # screen.show(True)
-
-    except:
-        print(__doc__)
+    screen = Screen(graphic_data)
+    screen.show(True)
 
 if __name__ == "__main__":
     main()
